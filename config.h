e8938442f4aa8cf5fa3a6a24b5328c66b8eded01
Oskar Szrajer <oskarszrajer@gmail.com>
2021-03-11 15:58:33 +0100

    cleaning and new patches

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /*  ____ _____  */
 /* |  _ \_   _|  Derek Taylor (DistroTube) */
-/* | | | || |  	http://www.youtube.com/c/DistroTube */
-/* | |_| || |  	http://www.gitlab.com/dwt1/ */
-/* |____/ |_|  	*/ 
+/* | | | || |   http://www.youtube.com/c/DistroTube */
+/* | |_| || |   http://www.gitlab.com/dwt1/ */
+/* |____/ |_|   */
 
 /* See LICENSE file for copyright and license details. */
 /* appearance */
@@ -19,45 +19,45 @@ static const int vertpadbar        = 7;   /* vertical padding for statusbar */
  * a hard dependency and makes colored fonts and emojis possible.
  */
 static const char *fonts[]     = {"Hack Nerd Font:style=Bold:size=10:antialias=true:autohint=true",
-	"JoyPixels:size=12:antialias=true:autohint=true"
+  "JoyPixels:size=12:antialias=true:autohint=true"
 };
 static const char col_1[]  = "#222222"; /* background color of bar */
 static const char col_2[]  = "#282c34"; /* border color unfocused windows */
 static const char col_3[]  = "#d7d7d7";
 static const char col_4[]  = "#005577"; /* border color focused windows and tags */
-/* bar opacity 
+/* bar opacity
  * 0xff is no transparency.
  * 0xee adds wee bit of transparency.
  * 0xdd adds adds a bit more transparency.
  * Play with the value to get desired transparency.
  */
-static const unsigned int baralpha    = 0xee; 
+static const unsigned int baralpha    = 0xee;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]        = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_3, col_1, col_2 },
-	[SchemeSel]  = { col_3, col_4, col_4 },
+  /*               fg         bg         border   */
+  [SchemeNorm] = { col_3, col_1, col_2 },
+  [SchemeSel]  = { col_3, col_4, col_4 },
 };
 static const unsigned int alphas[][3] = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+  /*               fg      bg        border     */
+  [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+  [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
 /* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
-static const char *tags[] = { "= 1: ", "= 2 : ", "= 3: ", "= 4: 🎝"};
+static const char *tags[] = { "= 1: ", "= 2 : ", "= 3: ", "= 4: 🎝", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      		instance    title       tags mask     isfloating   monitor */
-	{ "alacritty",  	NULL,       NULL,       1 << 0,       0,           -1 },
-	{ "Google-chrome",  	NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Slack",  		NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Spotify Premium", 	NULL,       NULL,       1 << 3,       0,           -1 },
+  /* xprop(1):
+   *  WM_CLASS(STRING) = instance, class
+   *  WM_NAME(STRING) = title
+   */
+  /* class          instance    title       tags mask     isfloating   monitor */
+  { "alacritty",    NULL,       NULL,       1 << 0,       0,           -1 },
+  { "Google-chrome",    NULL,       NULL,       1 << 1,       0,           -1 },
+  { "Slack",      NULL,       NULL,       1 << 2,       0,           -1 },
+  { "Spotify Premium",  NULL,       NULL,       1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
@@ -67,11 +67,11 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 #include "layouts.c"
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "[]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
-	{ "[G]",      grid }
+  /* symbol     arrange function */
+  { "[]=",      tile },    /* first entry is default */
+  { "[]",      NULL },    /* no layout function means floating behavior */
+  { "[M]",      monocle },
+  { "[G]",      grid }
 };
 
 /* key definitions */
@@ -90,67 +90,67 @@ static const char *dmenucmd[]    = { "dmenu_run", "-p", "Run: ", NULL };
 static const char *termcmd[]     = { "alacritty", NULL };
 
 static Key keys[] = {
-	/* modifier             chain key  key        function        argument */
-	{ MODKEY, 		-1,        XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,               -1,        XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,               -1,        XK_b,      togglebar,      {0} },
-	{ MODKEY|ShiftMask,     -1,        XK_j,      rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,     -1,        XK_k,      rotatestack,    {.i = -1 } },
-	{ MODKEY,               -1,        XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,               -1,        XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,               -1,        XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,               -1,        XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,               -1,        XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,               -1,        XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ControlMask,   -1,        XK_Return, zoom,           {0} },
-	{ MODKEY|ShiftMask,     -1,        XK_c,      killclient,     {0} },
+  /* modifier             chain key  key        function        argument */
+  { MODKEY,     -1,        XK_p,      spawn,          {.v = dmenucmd } },
+  { MODKEY,               -1,        XK_Return, spawn,          {.v = termcmd } },
+  { MODKEY,               -1,        XK_b,      togglebar,      {0} },
+  { MODKEY|ShiftMask,     -1,        XK_j,      rotatestack,    {.i = +1 } },
+  { MODKEY|ShiftMask,     -1,        XK_k,      rotatestack,    {.i = -1 } },
+  { MODKEY,               -1,        XK_j,      focusstack,     {.i = +1 } },
+  { MODKEY,               -1,        XK_k,      focusstack,     {.i = -1 } },
+  { MODKEY,               -1,        XK_i,      incnmaster,     {.i = +1 } },
+  { MODKEY,               -1,        XK_d,      incnmaster,     {.i = -1 } },
+  { MODKEY,               -1,        XK_h,      setmfact,       {.f = -0.05} },
+  { MODKEY,               -1,        XK_l,      setmfact,       {.f = +0.05} },
+  { MODKEY|ControlMask,   -1,        XK_Return, zoom,           {0} },
+  { MODKEY|ShiftMask,     -1,        XK_c,      killclient,     {0} },
 
-	/* Layout manipulation */
-	{ MODKEY,               -1,        XK_Tab,    cyclelayout,    {.i = -1 } },
-	{ MODKEY|ShiftMask,     -1,        XK_Tab,    cyclelayout,    {.i = +1 } },
-	{ MODKEY,               -1,        XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,     -1,        XK_space,  togglefloating, {0} },
+  /* Layout manipulation */
+  { MODKEY,               -1,        XK_Tab,    cyclelayout,    {.i = -1 } },
+  { MODKEY|ShiftMask,     -1,        XK_Tab,    cyclelayout,    {.i = +1 } },
+  { MODKEY,               -1,        XK_space,  setlayout,      {0} },
+  { MODKEY|ShiftMask,     -1,        XK_space,  togglefloating, {0} },
 
-	/* Switch to specific layouts */
-	{ MODKEY,               -1,        XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,               -1,        XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,               -1,        XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,               -1,        XK_g,      setlayout,      {.v = &layouts[3]} },
+  /* Switch to specific layouts */
+  { MODKEY,               -1,        XK_t,      setlayout,      {.v = &layouts[0]} },
+  { MODKEY,               -1,        XK_f,      setlayout,      {.v = &layouts[1]} },
+  { MODKEY,               -1,        XK_m,      setlayout,      {.v = &layouts[2]} },
+  { MODKEY,               -1,        XK_g,      setlayout,      {.v = &layouts[3]} },
 
-	{ MODKEY,               -1,        XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,     -1,        XK_0,      tag,            {.ui = ~0 } },
+  { MODKEY,               -1,        XK_0,      view,           {.ui = ~0 } },
+  { MODKEY|ShiftMask,     -1,        XK_0,      tag,            {.ui = ~0 } },
 
-	{ MODKEY,               -1,        XK_Print,      spawn,          CMD("/home/gotar/.local/bin/dmscrot") },
-	{ MODKEY,               -1,        XK_Pause,      spawn,          CMD("/home/gotar/.local/bin/dmlogout") },
+  { MODKEY,               -1,        XK_Print,      spawn,          CMD("/home/gotar/.local/bin/dmscrot") },
+  { MODKEY,               -1,        XK_Pause,      spawn,          CMD("/home/gotar/.local/bin/dmlogout") },
 
-	TAGKEYS(                -1,        XK_1,                      0)
-	TAGKEYS(                -1,        XK_2,                      1)
-	TAGKEYS(                -1,        XK_3,                      2)
-	TAGKEYS(                -1,        XK_4,                      3)
-	TAGKEYS(                -1,        XK_5,                      4)
-	TAGKEYS(                -1,        XK_6,                      5)
-	TAGKEYS(                -1,        XK_7,                      6)
-	TAGKEYS(                -1,        XK_8,                      7)
-	TAGKEYS(                -1,        XK_9,                      8)
-	{ MODKEY|ShiftMask,     -1,        XK_q,	quit,	      {0} },
-	{ MODKEY|ShiftMask,     -1,        XK_r,      	quit,         {1} }, 
+  TAGKEYS(                -1,        XK_1,                      0)
+    TAGKEYS(                -1,        XK_2,                      1)
+    TAGKEYS(                -1,        XK_3,                      2)
+    TAGKEYS(                -1,        XK_4,                      3)
+    TAGKEYS(                -1,        XK_5,                      4)
+    TAGKEYS(                -1,        XK_6,                      5)
+    TAGKEYS(                -1,        XK_7,                      6)
+    TAGKEYS(                -1,        XK_8,                      7)
+    TAGKEYS(                -1,        XK_9,                      8)
+    { MODKEY|ShiftMask,     -1,        XK_q,  quit,       {0} },
+    { MODKEY|ShiftMask,     -1,        XK_r,        quit,         {1} },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click           event mask   button          function        argument */
-	{ ClkLtSymbol,     0,           Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,     0,           Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,     0,           Button2,        zoom,           {0} },
-	{ ClkStatusText,   0,           Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,    MODKEY,      Button1,        movemouse,      {0} },
-	{ ClkClientWin,    MODKEY,      Button2,        togglefloating, {0} },
-	{ ClkClientWin,    MODKEY,      Button3,        resizemouse,    {0} },
-	{ ClkTagBar,       0,           Button1,        view,           {0} },
-	{ ClkTagBar,       0,           Button3,        toggleview,     {0} },
-	{ ClkTagBar,       MODKEY,      Button1,        tag,            {0} },
-	{ ClkTagBar,       MODKEY,      Button3,        toggletag,      {0} },
+  /* click           event mask   button          function        argument */
+  { ClkLtSymbol,     0,           Button1,        setlayout,      {0} },
+  { ClkLtSymbol,     0,           Button3,        setlayout,      {.v = &layouts[2]} },
+  { ClkWinTitle,     0,           Button2,        zoom,           {0} },
+  { ClkStatusText,   0,           Button2,        spawn,          {.v = termcmd } },
+  { ClkClientWin,    MODKEY,      Button1,        movemouse,      {0} },
+  { ClkClientWin,    MODKEY,      Button2,        togglefloating, {0} },
+  { ClkClientWin,    MODKEY,      Button3,        resizemouse,    {0} },
+  { ClkTagBar,       0,           Button1,        view,           {0} },
+  { ClkTagBar,       0,           Button3,        toggleview,     {0} },
+  { ClkTagBar,       MODKEY,      Button1,        tag,            {0} },
+  { ClkTagBar,       MODKEY,      Button3,        toggletag,      {0} },
 };
 
 
